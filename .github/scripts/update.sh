35dc8157d7c3c09830e084c1fde20748f9703aa9
niqdev <niqdev@users.noreply.github.com>
2021-07-22 06:27:16 +0100

    update log message

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -8,7 +8,7 @@ GIT_USER_EMAIL="my-awesome-bot@users.noreply.github.com"
 GIT_USER_NAME="my-awesome-bot"
 GIT_BRANCH="telegram-${TIMESTAMP//:/-}"
 PR_TITLE="[telegram-bot] $TIMESTAMP"
-PR_MESSAGE="Updates telegram messages"
+PR_MESSAGE="Updates telegram: $TIMESTAMP"
 
 ##############################
 
